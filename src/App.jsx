058d039064264c06b13afb4d25a03fafc9bab296
Mel <mel@innvertir.com>
2023-03-27 21:28:43 -0600

    :lipstick: Add JSX and CSS for App view, from graph-protocol examples. App displays example query data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ExampleQueryDocument, execute } from "../.graphclient/index";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState(null);
 
+  useEffect(() => {
+    execute(ExampleQueryDocument, {}).then((result) => {
+      setResult(result);
+      console.log(result);
+    });
+  }, []);
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Graph Client Example</p>
+        <fieldset>
+          {result?.data && (
+            <form>
+              <label>Data</label>
+              <br />
+              <textarea
+                value={JSON.stringify(result.data, null, 2)}
+                readOnly
+                rows={25}
+              />
+            </form>
+          )}
+          {result?.errors && (
+            <form>
+              <label>Error</label>
+              <br />
+              <textarea
+                value={JSON.stringify(result.errors, null, 2)}
+                readOnly
+                rows={25}
+              />
+            </form>
+          )}
+        </fieldset>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
