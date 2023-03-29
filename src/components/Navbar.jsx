d31f020da3725239a6f8160a2eb96eb92dfc58fa
Mel <mel@innvertir.com>
2023-03-29 11:06:24 -0600

    Add links to Navbar link components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,38 @@ import logo from "../logo.svg";
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <div className="logo-container">
+      <a
+        href="https://thegraph.com/"
+        className="logo-container"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={logo} className="the-graph-logo" alt="logo" />
         The Graph
-      </div>
+      </a>
       <div className="links-container">
-        <a className="link-element" href="/1">
+        <a
+          className="link-element"
+          href="https://thegraph.com/docs/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Docs
         </a>
-        <a className="link-element" href="/2">
+        <a
+          className="link-element"
+          href="https://github.com/graphprotocol/graph-client"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           The Graph Repo
         </a>
-        <a className="link-element" href="/3">
+        <a
+          className="link-element"
+          href="https://github.com/angelmc32/the-graph-client-react-tutorial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Tutorial Repo
         </a>
       </div>
